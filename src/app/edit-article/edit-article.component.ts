ed57043969d16bd062aef1ad8b353d6bdf81283f
Tammy Dang <dangitstammy@gmail.com>
2017-06-26 11:23:04 -0700

    create getArticleBodyById

--- src/app/edit-article/edit-article.component.ts
+++ src/app/edit-article/edit-article.component.ts
@@ -2,7 +2,7 @@ import { AuthService } from './../services/auth/auth.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ArticleService } from './../services/article/article.service';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-edit-article',
@@ -10,11 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-article.component.css']
 })
 export class EditArticleComponent implements OnInit {
-
+  @Input()
   id: any;
   routeParams: any;
   authInfo = null;
-  article: any;
+  inputArticle: any;
 
   constructor(
     private articleSvc: ArticleService,
@@ -32,10 +32,22 @@ export class EditArticleComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.id = params['id'];
       //this is where the article service will be called
+      this.articleSvc.getArticleById(this.id).subscribe(articleToEdit => {
+        let articleBodyId = articleToEdit.bodyId;
+        this.articleSvc.getArticleBodyById(articleBodyId).subscribe(articleBody => {
+          articleToEdit.body = articleBody.$value;
+          //create var for tag object and another variable for tag string
+          //loop through articleToEdit.tags and for each tag the tag string += tag.value+,
+          //set articleToEdit.tags = tags string 
+          this.inputArticle = articleToEdit;
+        })
+      });
     })
   }
 
-  edit(article){
-    console.log(article); 
+  edit(article, value){
+    console.log(article);
+    this.articleSvc.updateArticle(this.authInfo.$uid, article)
   }
+
 }
--- src/app/services/article/article.service.ts
+++ src/app/services/article/article.service.ts
@@ -5,13 +5,15 @@ import * as firebase from 'firebase';
 @Injectable()
 export class ArticleService {
 
-  articles: FirebaseObjectObservable<any[]>;
-  article: FirebaseListObservable<any>;
+  articles: FirebaseListObservable<any[]>;
+  article: FirebaseObjectObservable<any>;
   folder: any;
 
   constructor(
     private db: AngularFireDatabase
-  ) { }
+  ) {
+    this.articles = this.db.list('/articles') as FirebaseListObservable<ArticleService[]>
+   }
 
   getAllArticles() {
     return this.db.list('articleData/articles');
@@ -19,7 +21,11 @@ export class ArticleService {
 
   //getAllArticles by id
   getArticleById(id: string) {
-    return this.db.object('/articles/' + id);
+    return this.db.object('articleData/articles/' + id);
+  }
+  //getArticleBody which will also take an id
+  getArticleBodyById(id: string) {
+    return this.db.object('articleData/articleBodies/' + id);
   }
 
   createNewArticle(uid: string, article: any) {
@@ -54,13 +60,40 @@ export class ArticleService {
     }
   }
 
-  updateArticle(userid, article) {
-  //construct an object for the article to save
-
+  updateArticle(uid: string, article: any) {
+    //construct an object for the article to save
     //break tags into an array of objects and treat the tags like create articleSvc
+    console.log(article);
+    let tags = article.tags.replace(/\s/g, '').split(',');
+    let tagsObject = {};
+    for (let tag of tags) {
+      this.db.object(`articleData/tags/${tag}`)
+        .take(1)
+        .subscribe(data => {
+          if (!data.$value)
+            this.db.object(`articleData/tags/${tag}`).set(firebase.database.ServerValue.TIMESTAMP);
+        });
+      tagsObject[tag] = true;
+    }
     //create a new article body with a new key and save the key
+    let bodyKey = this.db.list('articleData/articleBodies').push(article.body).key;
+    let articleToUpdate = {
+      title: article.title,
+      bodyId: bodyKey,
+      tags: tagsObject,
+      version: article.version +1,
+      lastUpdated: firebase.database.ServerValue.TIMESTAMP
+    }
+    let articleKey = this.db.list('articleData/articles').push(articleToUpdate).key;
+    this.db.object(`articleData/editorsPerArticle/${articleKey}/${uid}`).set(true);
+    this.db.object(`articleData/articlesPerEditor/${uid}/${articleKey}`).set(true);
+      for (let tag of tags) {
+        this.db.object(`articleData/articlesPerTag/${tag}/${articleKey}`).set(true);
+      }
+      return this.articles.update(uid, article);
+    }
     //do not include the articlesPerAuthor key
     //you might need to include a version number if it's not included
     //create a loop for the tag of tags and for each of the tags you have to see if the tag
+    //increment versions, articleToUpdate = article.version +1
   }
-}
