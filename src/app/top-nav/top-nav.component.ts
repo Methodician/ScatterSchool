5fe6fdcddbc35123994ae018aca12c81945abff3
Kyle Lange <baronsintrees@gmail.com>
2017-07-12 15:25:49 -0700

    add nav fixes for scroll styles and all responsive conditions.

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.css']
+  styleUrls: ['./top-nav.component.scss']
 })
 export class TopNavComponent implements OnInit {
   isCollapsed: boolean = true;
