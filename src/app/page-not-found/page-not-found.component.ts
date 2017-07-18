a7cf30d42a927430af441df0d4e7033d440e9c40
Kyle Lange <baronsintrees@gmail.com>
2017-07-18 15:50:31 -0700

    add png xOut to navigate from 404 page back to home.  Broken.

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'page-not-found',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  navigateHome() {
+    this.router.navigate(['/home']);
   }
 
 }
