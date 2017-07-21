cb3db0c4a4488bd8b686911ae00663d1a9c56574
Methodician <jacobgjohnston@gmail.com>
2017-07-20 17:52:45 -0700

    set up editing article detail preview might still add previews for preview tiles...

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ArticleService } from './../services/article/article.service';
 
 @Component({
-  selector: 'article-detail',
+  selector: 'app-article-detail',
   templateUrl: './article-detail.component.html',
   styleUrls: ['./article-detail.component.scss']
 })
@@ -11,6 +11,7 @@ export class ArticleDetailComponent implements OnInit {
   articleKey: string;
   isArticleFeatured: boolean;
   @Input() articleData: any;
+  @Input() editingPreview = false;
   author;
   article;
   constructor(
@@ -20,24 +21,28 @@ export class ArticleDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    if (!this.editingPreview) {
+      this.route.params.subscribe(params => {
+        if (params['id'])
+          this.articleKey = params['id'];
+        this.checkIfFeatured();
+        this.getArticleData();
+      });
+    }
+    else {
+      this.checkIfFeatured();
+      this.getArticleBody(this.articleData);
+    }
+  }
 
-    this.route.params.subscribe(params => {
-      if (params['id'])
-        this.articleKey = params['id'];
-      this.articleSvc.isArticleFeatured(this.articleKey).subscribe(featured => {
-        this.isArticleFeatured = featured;
-      });
-      this.articleSvc.getArticleById(this.articleKey).subscribe(articleData => {
-        this.articleSvc.getArticleBodyById(articleData.bodyId).subscribe(articleBody => {
-          articleData.body = articleBody.$value;
-          this.article = articleData;
-        });
-        this.articleSvc.getAuthorById(articleData.authorId).subscribe(author => {
-          this.author = author;
-        });
-        //this.article = articleData;
-      });
-    });
+  ngOnChanges(changes: SimpleChanges) {
+    //  Must make sure form is initalized before checking...
+    if (changes['articleData'] && changes['articleData'].currentValue) {
+      //if (changes['initialValue']) {
+      // We have two methods to set a form's value: setValue and patchValue.
+      this.article = changes['articleData'].currentValue;
+    }
+
   }
 
   navigateToAuthor() {
@@ -53,6 +58,32 @@ export class ArticleDetailComponent implements OnInit {
       this.articleSvc.unsetFeaturedArticle(this.articleKey);
     else
       this.articleSvc.setFeaturedArticle(this.articleKey);
+  }
+
+  checkIfFeatured() {
+    this.articleSvc.isArticleFeatured(this.articleKey).subscribe(featured => {
+      this.isArticleFeatured = featured;
+    });
+  }
+
+  getArticleData() {
+    this.articleSvc.getArticleById(this.articleKey).subscribe(articleData => {
+      this.getArticleBody(articleData);
+      this.getAuthor(articleData.authorId);
+    });
+  }
+
+  getArticleBody(articleData: any) {
+    this.articleSvc.getArticleBodyById(articleData.bodyId).subscribe(articleBody => {
+      articleData.body = articleBody.$value;
+      this.article = articleData;
+    });
+  }
+
+  getAuthor(authorId: string) {
+    this.articleSvc.getAuthorById(authorId).subscribe(author => {
+      this.author = author;
+    });
   }
 
 }
