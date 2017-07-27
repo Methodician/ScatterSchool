e0fb15b446f250e3802fba1c43e333887f2e1b52
Kyle Lange <baronsintrees@gmail.com>
2017-07-27 13:09:58 -0700

    add sign in and sign up functionality to the footer component.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserInfoOpen } from './../services/user/user-info';
+import { UserService } from './../services/user/user.service';
+import { AuthService } from './../services/auth/auth.service';
+import { AuthInfo } from './../services/auth/auth-info';
 
 @Component({
   selector: 'footer',
@@ -8,9 +12,27 @@ import { Router } from '@angular/router';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  authInfo: AuthInfo = new AuthInfo(null, false);
+  displayName: string = '';
+
+  constructor(
+    private authSvc: AuthService,
+    private userSvc: UserService
+  ) { }
 
   ngOnInit() {
+    this.authSvc.authInfo$.subscribe(authInfo => {
+      this.authInfo = authInfo;
+    });
+    this.userSvc.userInfo$.subscribe((userInfo: UserInfoOpen) => {
+      if (userInfo && userInfo.$key) {
+        this.displayName = userInfo.alias || userInfo.fName;
+      }
+    });
+  }
+
+  logout() {
+    this.authSvc.logout();
   }
 
 }
