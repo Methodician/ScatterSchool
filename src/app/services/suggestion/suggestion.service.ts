85a40cf1df6560c920141fc1d273660e09f8b80e
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-08-16 13:15:11 -0700

    initial commit

--- src/app/services/suggestion/suggestion.service.ts
+++ src/app/services/suggestion/suggestion.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+
 
 @Injectable()
 export class SuggestionService {
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase,
+  ) { }
+
+  getAllSuggestions() {
+    return this.db.list('suggestionData/suggestions').map
+  };
+
 
 }
