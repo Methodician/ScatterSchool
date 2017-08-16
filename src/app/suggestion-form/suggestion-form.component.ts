595ffb25cef6770ab759af1be8dc194fe66191e8
Aubrey Masten <mastenaub@gmail.com>
2017-08-16 16:05:50 -0700

    add suggestion form validations

--- src/app/suggestion-form/suggestion-form.component.ts
+++ src/app/suggestion-form/suggestion-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'suggestion-form',
@@ -10,15 +10,26 @@ export class SuggestionFormComponent {
 
   form: FormGroup;
   
-    constructor(fb: FormBuilder) {
-      this.form = fb.group({
-        title: '',
-        pitch: ''
-      });
-     }
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      title: ['', [Validators.required, Validators.maxLength(140)]],
+      pitch: ['', Validators.required]
+    });
+  }
+
+  get title() {
+    return this.form.get('title');
+  }
+
+  get pitch() {
+    return this.form.get('pitch');
+  }
+
+  get valid() {
+    return this.form.valid
+  }
 
   get value() {
     return this.form.value
   }
-
 }
