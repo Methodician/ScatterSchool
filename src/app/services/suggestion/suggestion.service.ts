fd06ee8380553f9b005be9229775973b7df205f3
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-08-16 15:10:40 -0700

    add suggestion service getAllSuggestions

--- src/app/services/suggestion/suggestion.service.ts
+++ src/app/services/suggestion/suggestion.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 
 @Injectable()
 export class SuggestionService {
+  suggestions: FirebaseListObservable<any[]>;
 
-  constructor(
-    private db: AngularFireDatabase,
-  ) { }
+  constructor(private db: AngularFireDatabase      
+  ) { this.suggestions = db.list('suggestions');
+  }
 
   getAllSuggestions() {
-    return this.db.list('suggestionData/suggestions').map
+    return this.db.list('suggestionData/suggestions');
   };
-
-
 }
--- src/app/suggestion-preview/suggestion-preview.component.ts
+++ src/app/suggestion-preview/suggestion-preview.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SuggestionService } from './../services/suggestion/suggestion.service'
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-suggestion-preview',
   templateUrl: './suggestion-preview.component.html',
-  styleUrls: ['./suggestion-preview.component.scss']
+  styleUrls: ['./suggestion-preview.component.scss'],
+  providers: [ SuggestionService ]
 })
+
 export class SuggestionPreviewComponent implements OnInit {
-  @Input() suggestionData: any;
+  suggestions: FirebaseListObservable<any[]>;
   
-  constructor() { }
+  constructor(private router: Router, private suggestionService: SuggestionService) { }
 
   ngOnInit() {
+    this.suggestions = this.suggestionService.getAllSuggestions();
   }
-
 }
