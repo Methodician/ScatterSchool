156e8a25727bb81e0716e1fa9d43b0913e9828ba
Aubrey Masten <mastenaub@gmail.com>
2017-08-16 10:20:31 -0700

    add placeholder suggestion data

--- src/app/suggestions/suggestions.component.ts
+++ src/app/suggestions/suggestions.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suggestions.component.scss']
 })
 export class SuggestionsComponent implements OnInit {
-
+  // placeholder data until getAllSuggestions service method is implemented
+  suggestions = [{
+    title: "I would like to build a fast car.",
+    pitch: "Donec mattis efficitur scelerisque. Phasellus sit amet enim cursus, pretium lectus vel, posuere tortor."
+  },{
+    title: "Design a more efficient solar panel",
+    pitch: "Donec mattis efficitur scelerisque. Phasellus sit amet enim cursus, pretium lectus vel, posuere tortor."
+  }]
+  
   constructor() { }
 
   ngOnInit() {
