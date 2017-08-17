9bf5607e2bf6393319644a19d405754a57f6eeda
Aubrey Masten <mastenaub@gmail.com>
2017-08-17 15:29:19 -0700

    link suggestion service to add suggestion form

--- src/app/add-suggestion/add-suggestion.component.ts
+++ src/app/add-suggestion/add-suggestion.component.ts
@@ -1,3 +1,4 @@
+import { SuggestionService } from './../services/suggestion/suggestion.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,9 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AddSuggestionComponent {
 
-  constructor() {}
+  constructor(private service: SuggestionService) {}
 
    saveSuggestion(formData) {
-     console.log('data', formData);
+     this.service.saveSuggestion(formData);
    }
 }
