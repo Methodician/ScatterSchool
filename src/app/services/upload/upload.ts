ac826e2d7cc781f9ed202396948e361f0b708297
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-08-18 17:46:48 -0700

    upload html form

--- src/app/services/upload/upload.ts
+++ src/app/services/upload/upload.ts
@@ -2,8 +2,8 @@ export class Upload {
   public $key: string;
   public file:File;
   public url:string;
-  public lastUpdated: number,
-  public timeStamp: number,
+  public lastUpdated: number;
+  public timeStamp: number;
   constructor(file:File) {
     this.file = file;
   }
