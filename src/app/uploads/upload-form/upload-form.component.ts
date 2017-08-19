86eaeb31275b50ab827a0ab585402a2768df71a7
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-08-18 18:19:01 -0700

    upload file to firebase storage

--- src/app/uploads/upload-form/upload-form.component.ts
+++ src/app/uploads/upload-form/upload-form.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadService } from '../../services/upload/upload.service';
+import { Upload } from '../../services/upload/upload';
+
 
 @Component({
   selector: 'app-upload-form',
   templateUrl: './upload-form.component.html',
-  styleUrls: ['./upload-form.component.scss']
+  styleUrls: ['./upload-form.component.scss'],
+  providers: [ UploadService ]
 })
-export class UploadFormComponent implements OnInit {
 
-  constructor() { }
+export class UploadFormComponent implements OnInit {
+  selectedFiles: FileList;
+  currentUpload: Upload;
+
+  constructor(private upSvc: UploadService) { }
+
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadSingle() {
+    let file = this.selectedFiles.item(0)
+    this.currentUpload = new Upload(file);
+    this.upSvc.pushUpload(this.currentUpload)
+  }
 
   ngOnInit() {
   }
