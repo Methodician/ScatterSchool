5ac33e6ef1240210e9ec692b01af27122823c3aa
Aubrey Masten <mastenaub@gmail.com>
2017-08-24 14:04:51 -0700

    connect vote component to service, add validation to input and interaction

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,7 +1,8 @@
 import { Router } from '@angular/router';
 import { SuggestionService } from 'app/services/suggestion/suggestion.service';
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { Vote } from "app/services/suggestion/vote";
+import { VoteService } from "app/services/vote/vote.service";
+import { Vote } from "app/services/vote/vote";
 
 @Component({
   selector: 'vote',
@@ -12,35 +13,38 @@ export class VoteComponent implements OnInit, OnChanges {
   @Input() suggestion;
   @Input() currentUserKey;
   voteState = 0;
-  totalVotes = 0;
+  voteTotal = 0;
 
-  constructor(private service: SuggestionService, private router: Router) { }
+  constructor(private voteSvc: VoteService, private router: Router) { }
 
   ngOnInit() {
-    // can we add subscriptions in the service? I would like to avoid subscribing to every individual vote state.
-    if(this.currentUserKey) {
-      this.service.getSuggestionVoteStateByUser(this.suggestion.$key, this.currentUserKey).subscribe(voteStateData => {
-        this.voteState = (voteStateData.$value) ? voteStateData.$value : 0;
-      });
-    }
+    this.voteSvc.getSuggestionVoteStateByUser(this.suggestion.$key, this.currentUserKey).subscribe(voteStateData => {
+      this.voteState = (voteStateData.$value) ? voteStateData.$value : 0;
+    });
+    // use if receiving suggestion key as input
+    // this.voteSvc.getTotalVotesBySuggestion(this.suggestion.$key).subscribe(out => {
+    //   this.voteTotal = out.$value;
+    // })
   }
 
+  // use if receiving suggestion object as input from parent subscription
   ngOnChanges(changes) {
-    this.totalVotes = changes.suggestion.currentValue.voteCount;
+    if(changes.suggestion) this.voteTotal = changes.suggestion.currentValue.voteCount; 
+  }
+
+  attemptVote(voteNum) {
+    if (this.currentUserKey) this.vote(voteNum);
+    else this.router.navigate(['login']);
   }
 
   // sets the current state of a user's vote
   // then calls a method to update the database
   // voteNum represents either a positive or negative vote
   vote(voteNum) {
-    if(this.currentUserKey){
-      this.totalVotes += this.getTotalVoteChange(voteNum)
+      this.voteTotal += this.getTotalVoteChange(voteNum)
       // resets the component's vote state to 0 if the current vote state is equal to the vote that was just clicked
       this.voteState = (this.voteState === voteNum) ? 0 : voteNum;
       this.saveVote();
-    } else {
-      this.router.navigate(['login'])
-    }
   }
 
   getTotalVoteChange(voteNum){
@@ -53,8 +57,8 @@ export class VoteComponent implements OnInit, OnChanges {
 
   // creates new vote object containing current user, suggestion, and vote state data
   saveVote() {
-    let vote = new Vote(this.currentUserKey, this.suggestion.$key, this.voteState, this.totalVotes);
-    this.service.makeVote(vote);
+    let vote = new Vote(this.currentUserKey, this.suggestion.$key, this.voteState, this.voteTotal);
+    this.voteSvc.makeVote(vote);
   }
 
   // returns boolean, true if given voteState equals current component voteState
