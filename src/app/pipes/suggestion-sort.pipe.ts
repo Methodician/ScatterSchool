b3bcdf4944c6611e3393a1a91ab891a384b3e70c
Aubrey Masten <mastenaub@gmail.com>
2017-08-24 14:51:24 -0700

    add sorting by oldest and newest suggestions

--- src/app/pipes/suggestion-sort.pipe.ts
+++ src/app/pipes/suggestion-sort.pipe.ts
@@ -7,11 +7,18 @@ export class SuggestionSortPipe implements PipeTransform {
 
   transform(suggestions: any, sortType?: any): any {
     if(!suggestions) return null;
-    
     switch(sortType){
       case 'upvotes':
         return suggestions.sort((a, b) => {
-          return b.voteCount - a.voteCount
+          return b.voteCount - a.voteCount;
+        });
+      case 'newest':
+        return suggestions.sort((a,b) => {
+          return b.lastUpdated - a.lastUpdated;
+        });
+      case 'oldest':
+        return suggestions.sort((a,b) => {
+          return a.lastUpdated - b.lastUpdated;
         });
       default:
         return suggestions;
