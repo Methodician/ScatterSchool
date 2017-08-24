0000fcf85cd436f54d41f45722e5e94563621859
Aubrey Masten <mastenaub@gmail.com>
2017-08-24 14:03:22 -0700

    remove unnecessary vote import from suggestion-preview component

--- src/app/suggestion-preview/suggestion-preview.component.ts
+++ src/app/suggestion-preview/suggestion-preview.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 import { SuggestionService } from './../services/suggestion/suggestion.service'
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Router } from '@angular/router';
-import { Vote } from "app/services/suggestion/vote";
 
 @Component({
   selector: 'app-suggestion-preview',
