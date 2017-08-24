0952ec4876b6faceb2533f132c709b88ed9236a1
Aubrey Masten <mastenaub@gmail.com>
2017-08-24 14:02:19 -0700

    remove vote related code from suggestion service

--- src/app/services/suggestion/suggestion.service.ts
+++ src/app/services/suggestion/suggestion.service.ts
@@ -2,16 +2,12 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from "rxjs/Observable";
 import * as firebase from 'firebase';
-import { Vote } from "app/services/suggestion/vote";
 
 
 @Injectable()
 export class SuggestionService {
-  suggestions: FirebaseListObservable<any[]>;
-
-  constructor(private db: AngularFireDatabase      
-  ) { this.suggestions = db.list('suggestions');
-  }
+  
+  constructor(private db: AngularFireDatabase) {}
 
   getAllSuggestions() {
     return this.db.list('suggestionData/suggestions');
@@ -36,20 +32,5 @@ export class SuggestionService {
 
     let dbSuggestion = this.getSuggestionByKey(key);
     dbSuggestion.update(paramsToUpdate);
-  }
- 
-  getSuggestionVoteStateByUser(suggestionKey, userKey) {
-    return this.db.object(`suggestionData/suggestionVotesPerUser/${userKey}/${suggestionKey}`);
-  }
-
-  makeVote(vote: Vote){
-    this.db.object(`suggestionData/suggestionVotesPerUser/${vote.userKey}/${vote.suggestionKey}`).set(vote.getDbVoteStatus());
-    this.db.object(`suggestionData/userVotesPerSuggestion/${vote.suggestionKey}/${vote.userKey}`).set(vote.getDbVoteStatus());
-    this.setTotalVotes(vote.suggestionKey, vote.voteTotal)
-  }
-
-  setTotalVotes(suggestionKey, voteTotal) {
-    this.db.object(`suggestionData/suggestions/${suggestionKey}`).update({voteCount: voteTotal})
-    
   }
 }
