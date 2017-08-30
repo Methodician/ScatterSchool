a3fc89bb4faa476d4f0956bb1d182f6f3a340366
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-08-30 16:15:16 -0700

    styled profileImage component

--- src/app/profile-image/profile-image.component.ts
+++ src/app/profile-image/profile-image.component.ts
@@ -1,6 +1,4 @@
-import { AuthInfo } from './../services/auth/auth-info';
 import { AuthService } from 'app/services/auth/auth.service';
-import { UserService } from './../services/user/user.service';
 import { UploadService } from './../services/upload/upload.service';
 import { Component, OnInit } from '@angular/core';
 import { Upload } from './../services/upload/upload';
@@ -13,37 +11,39 @@ import { Upload } from './../services/upload/upload';
 })
 export class ProfileImageComponent implements OnInit {
   selectedFiles;
+  uid;
+  profileImageUrl;
 
   constructor(
     private uploadSvc: UploadService,
-    private userSvc: UserService,
     private authSvc: AuthService
   ) { }
 
   ngOnInit() {
-    window.scrollTo(0, 0);
     this.authSvc.authInfo$.subscribe(info => {
-      const uid = info.$uid;
-      this.getProfileImage(uid);
+      this.uid = info.$uid;
+      this.getProfileImage(this.uid);
     });
   }
 
   getProfileImage(uid) {
     this.uploadSvc.getProfileImage(uid).subscribe(profileData => {
       if (profileData.url) {
-        const profileImageUrl = profileData.url;
+        this.profileImageUrl = profileData.url;
       }
     });
   }
 
   detectFiles(event) {
     this.selectedFiles = event.target.files;
+    console.log(event);
   }
 
   uploadImage() {
     const file = this.selectedFiles.item(0);
     const currentUpload = new Upload(file);
     this.uploadSvc.pushUpload(currentUpload);
+    console.log(currentUpload);
   }
 }
 
