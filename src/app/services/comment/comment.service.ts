b2eea3a00279afe2a67c88a34cf117792f9c69da
Aubrey Masten <mastenaub@gmail.com>
2017-08-30 17:16:00 -0700

    add additional comment/article/reply associations when saving any comment

--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -14,14 +14,37 @@ export class CommentService {
   saveComment(commentData) {
     let commentToSave = {
       authorKey: commentData.authorKey,
+      parentKey: commentData.parentKey,
       text: commentData.text,
-      timestamp: firebase.database.ServerValue.TIMESTAMP
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
     }
 
     let dbSaveData = this.db.list('commentData/comments').push(commentToSave);
     
     this.makeParentAssociation(commentData.parentKey, dbSaveData.key);
-    this.makeUserAssociation(commentData.authorKey, dbSaveData.key);    
+    this.makeUserAssociation(commentData.authorKey, dbSaveData.key);
+    this.makeParentTypeAssociation(commentData.parentType, commentData.parentKey, dbSaveData.key);
+    this.makeCommentsPerArticleAssociation(commentData.parentKey, dbSaveData.key);
+  }
+
+  makeParentTypeAssociation(parentType, parentKey, childKey) {
+    if (parentType === 'article') this.makeCommentsPerArticleAssociation(parentKey, childKey);
+    if (parentType === 'comment') this.makeRepliesPerCommentAssociation(parentKey, childKey);
+  }
+
+  makeRepliesPerCommentAssociation(parentCommentKey, replyCommentKey) {
+    this.db.object(`commentData/repliesPerComment/${parentCommentKey}/${replyCommentKey}`).set(true);
+  }
+
+  getCommentsByParentKey(parentKey) {
+    return this.db.list(`commentData/comments`, {query: {
+      orderByChild: 'parentKey',
+      equalTo: parentKey
+    }});
+  }
+
+  makeCommentsPerArticleAssociation(articleKey, childCommentKey) {
+    this.db.object(`commentData/commentsPerArticle/${articleKey}/${childCommentKey}`).set(true);
   }
 
   makeParentAssociation(parentKey, childCommentKey) {
