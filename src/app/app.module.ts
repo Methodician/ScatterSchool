d631911344fa4490ea0f611f39ed8f613b156b31
Joe Parsley <joewparsley@gmail.com>
2017-09-21 15:10:35 -0700

    install angular material, install button module, convert follow-btn.component.html to use angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {MdButtonModule} from '@angular/material';
 import { UploadService } from './services/upload/upload.service';
 import { Upload } from './services/upload/upload';
 import { CharacterCounterComponent } from './character-counter/character-counter.component';
@@ -122,7 +123,7 @@ import { AllArticlesComponent } from './all-articles/all-articles.component';
     TimeElapsedPipe,
     UploadFormComponent,
     AllArticlesComponent,
-    ArticleCoverImageComponent
+    ArticleCoverImageComponent,
   ],
   imports: [
     BrowserModule,
@@ -135,7 +136,8 @@ import { AllArticlesComponent } from './all-articles/all-articles.component';
     AngularFireAuthModule,
     CKEditorModule,
     TagInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdButtonModule
   ],
   providers: [
     AngularFireDatabase,
