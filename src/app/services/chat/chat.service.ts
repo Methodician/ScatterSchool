6308af1dcb55a2a7caf120d1a7d2b60efa63c6a8
Aubrey Masten <mastenaub@gmail.com>
2017-09-27 10:14:08 -0700

    remove outdated chat service code

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -41,40 +41,19 @@ export class ChatService {
       });
   }
 
-  // getMessagesByRecipientAndAuthor(recipientKey, authorKey) {
-  //   this.db.list(`chatData/messagesPerRecipientPerAuthor/${authorKey}/${recipientKey}`)
-  //     .map(messagesPerKey => messagesPerKey
-  //       .map(message =>
-  //         this.db.object(`chatData/messages/${message.$key}`)
-  //       ))
-  //     .flatMap(firebaseObjects =>
-  //       Observable.combineLatest(firebaseObjects));
-  // }
-
   saveMessage(messageData) {
-    /* let messageToSave = {
-      text: messageData.text,
-      authorKey: messageData.authorKey,
-      authorName: messageData.authorName,
-      timestamp: firebase.database.ServerValue.TIMESTAMP
-    } */
     messageData.timestamp = firebase.database.ServerValue.TIMESTAMP;
 
     let dbMessageRef = this.getMessagesForCurrentChat().push(messageData);
-
-    //let dbMessageRef = this.getAllMessages().push(messageToSave);
-    //this.saveMessageRecipientAuthorAssociation(dbMessageRef.key, messageData.recipientKey, messageData.authorKey)
   }
-
-  // getAllMembersPerChat() {
-  //   return this.db.list()
-  // }
 
   createChat(users) {
     let dbChatsRef = this.getAllChats();
     let chatKey = dbChatsRef.push({ timestamp: firebase.database.ServerValue.TIMESTAMP }).key;
+
     this.currentChatKey = chatKey;
     this.currentChatKey$.next(chatKey);
+    
     for (let user of users) {
       let displayName = user.alias ? user.alias : user.fName;
       this.db.object(`chatData/chats/${chatKey}/members/${user.$key}`).update({name: displayName});
@@ -87,9 +66,4 @@ export class ChatService {
     this.currentChatKey = chatKey;
     this.currentChatKey$.next(chatKey);
   }
-
-  // saveMessageRecipientAuthorAssociation(messageKey, recipientKey, authorKey) {
-  //   this.db.object(`chatData/messagesPerRecipientPerAuthor/${authorKey}/${recipientKey}/${messageKey}`).set(true);
-  // }
-
 }
