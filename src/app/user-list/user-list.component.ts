fd49702e108dc85b967f3766661cf9c39a67d066
Aubrey Masten <mastenaub@gmail.com>
2017-09-27 10:11:14 -0700

    clean and comment user list

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -11,12 +11,13 @@ import { UserService } from './../services/user/user.service';
 export class UserListComponent implements OnInit {
   userList;
   chatList;
-  loggedInUser: UserInfoOpen;
   currentChat;
+  loggedInUser: UserInfoOpen;
+
   constructor(
     private userSvc: UserService,
     private chatSvc: ChatService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.userSvc.getUserList().subscribe(userList => {
@@ -35,54 +36,69 @@ export class UserListComponent implements OnInit {
       if(key) {
         this.chatSvc.getChatByKey(key).subscribe(chat => {
           this.currentChat = chat;
-        })
+        });
       }
     });
   }
 
-  isNotInCurrentChat(userKey) {
-    return !this.currentChat.members[userKey];
-  }
-
-  addToChat(user, event) {
-    event.stopPropagation();
-    let users = Object.keys(this.currentChat.members).map(memberKey => {
-      return {
-        alias: this.currentChat.members[memberKey].name,
-        $key: memberKey
-      }
-    });
-    users.push(user);
-    let existingChat = this.findChat(users)
-    existingChat ? this.openChat(existingChat.$key) : this.createChat(users);
-  }
-
-  memberNames(chat) {
-    return (<any>Object).values(chat.members).map(member => {return member.name}).join(', ');
-  }
-
-  displayName(user) {
-    return user.alias ? user.alias : user.fName;
-  }
-
-  findChat(users) {
-    if(!this.chatList) return false;
-    return this.chatList.filter(chat => {
-      return users.length === Object.keys(chat.members).length && users.every(user => chat.members[user.$key])
-    })[0];
-  }
-
-  createOrOpenChat(users) {
-    users.push(this.loggedInUser);
-    let existingChat = this.findChat(users);
-    (existingChat) ? this.openChat(existingChat.$key) : this.createChat(users);
+  createChat(users) {
+    this.chatSvc.createChat(users);
   }
 
   openChat(chatKey){
     this.chatSvc.openChat(chatKey);
   }
 
-  createChat(users) {
-    this.chatSvc.createChat(users);
+  // this code will break if there is no currently logged in user
+  // would prefer to leave as is, and not render the user list entirely until
+  // the user has logged on
+  createOrOpenChat(users) {
+    let existingChat = this.findExistingChat(users);
+    (existingChat) ? this.openChat(existingChat.$key) : this.createChat(users);
+  }
+
+  //note: code is intentionally verbose, can be shortened if necessary
+  findExistingChat(queriedUserList) {
+    if(!this.chatList) return false;
+
+    return this.chatList.filter(chat => {
+      let chatMembersLength = Object.keys(chat.members).length;
+      // chat is identical if the queriedUserList is of the same length and contains every memberKey that chat.members contains
+      return queriedUserList.length === chatMembersLength  && queriedUserList.every(user => chat.members[user.$key])
+    })[0];
+    // ^ we can guarantee that there will never be identical chats the the database, so we always
+    // return the first (and only) chat from the filtered array
+  }
+
+  addUserToChat(user, event) {
+    event.stopPropagation();
+    let users = this.membersObjectToUsersArray(this.currentChat.members);
+    users.push(user);
+    this.createOrOpenChat(users)
+  }
+
+  // The current POC compromise. We are accessing data on current chat users
+  // from the currentChat object members node. The naming conventions are different
+  // from the UserInfoOpen class we recieve from the userList, but provide enough
+  // data to create the necessary associations in the database (and use the same service method).
+  membersObjectToUsersArray(members) {
+    return Object.keys(this.currentChat.members).map(memberKey => {
+      return {
+        alias: this.currentChat.members[memberKey].name,
+        $key: memberKey
+      }
+    });
+  }
+  
+  isNotInCurrentChat(userKey) {
+    return !this.currentChat.members[userKey];
+  }  
+
+  getMemberNames(chat) {
+    return (<any>Object).values(chat.members).map(member => member.name).join(', ');
+  }
+
+  displayName(user) {
+    return user.alias ? user.alias : user.fName;
   }
 }
