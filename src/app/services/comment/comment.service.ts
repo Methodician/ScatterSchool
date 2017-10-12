5396a09f80b4f1232a34a3f27a69f3c53269088e
Aubrey Masten <mastenaub@gmail.com>
2017-10-11 18:46:56 -0700

    add methods to track comment count for articles and comments

--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -15,9 +15,11 @@ export class CommentService {
     let commentToSave = {
       authorKey: commentData.authorKey,
       parentKey: commentData.parentKey,
+      parentType: commentData.parentType,
       text: commentData.text,
       timestamp: firebase.database.ServerValue.TIMESTAMP,
-      lastUpdated: firebase.database.ServerValue.TIMESTAMP
+      lastUpdated: firebase.database.ServerValue.TIMESTAMP,
+      commentCount: 0
     }
 
     let dbSaveData = this.db.list('commentData/comments').push(commentToSave);
@@ -26,6 +28,34 @@ export class CommentService {
     this.makeUserAssociation(commentData.authorKey, dbSaveData.key);
     this.makeParentTypeAssociation(commentData.parentType, commentData.parentKey, dbSaveData.key);
     this.makeCommentsPerArticleAssociation(commentData.parentKey, dbSaveData.key);
+    this.updateCommentCount(commentData.parentKey, commentData.parentType, 1)
+  }
+
+  // this could be replaced with an enum
+  getBasePathByParentType(parentType) {
+    switch(parentType) {
+      case "comment":
+        return "commentData/comments/"
+      case "article":
+        return "articleData/articles/"
+      default:
+        return false;
+    }
+  }
+
+  updateCommentCount(parentKey, parentType, value) {
+    let parentPath = this.getBasePathByParentType(parentType) + parentKey;
+
+    this.db.object(parentPath).$ref.ref.transaction(parent => {
+      if (parent) {
+        // logic is verbose, but accounts for current data/data added which has not comment count
+        if (parent.commentCount) parent.commentCount += value;
+        else if (value == 1) parent.commentCount = 1;
+        else if (value == -1) parent.commentCount = 0; 
+        if(parentType == "comment") this.updateCommentCount(parent.parentKey, parent.parentType, value);
+      }
+      return parent;
+    });    
   }
 
   updateComment(newCommentData) {
@@ -36,8 +66,9 @@ export class CommentService {
     this.db.object(`commentData/comments/${newCommentData.key}`).update(commentDataToUpdate)
   }
 
-  deleteComment(commentKey) {
-    this.db.object(`commentData/comments/${commentKey}`).update({isDeleted: true});
+  deleteComment(comment) {
+    this.db.object(`commentData/comments/${comment.$key}`).update({isDeleted: true});
+    this.updateCommentCount(comment.parentKey, comment.parentType, -1);
   }
 
   makeParentTypeAssociation(parentType, parentKey, childKey) {
