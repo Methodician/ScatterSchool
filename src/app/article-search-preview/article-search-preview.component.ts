134cd8952f7093d96b3e8896437af193358b622c
JordLoop <JordLoop@gmail.com>
2017-10-12 14:27:38 -0700

    add system for bookmarking to article-search-preview.component

--- src/app/article-search-preview/article-search-preview.component.ts
+++ src/app/article-search-preview/article-search-preview.component.ts
@@ -2,6 +2,8 @@ import { UploadService } from './../services/upload/upload.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ArticleService } from './../services/article/article.service';
+import { UserService } from './../services/user/user.service';
+import { AuthService } from './../services/auth/auth.service';
 
 @Component({
   selector: 'article-search-preview',
@@ -13,18 +15,30 @@ export class ArticleSearchPreviewComponent implements OnInit {
   author;
   profileImageUrl;
   articleCoverImageUrl;
+  user;
+  isArticleBookmarked;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private articleSvc: ArticleService,
-    private uploadSvc: UploadService
+    private uploadSvc: UploadService,
+    private userSvc: UserService,
+    private authSvc: AuthService
   ) { }
 
   ngOnInit() {
     this.articleSvc.getAuthorByKey(this.articleData.authorKey).subscribe(author => {
       this.author = author;
     });
+    // ----------------------
+    this.userSvc.userInfo$.subscribe(user => {
+      if (user) {
+        this.user = user;
+        this.checkIfBookmarked();
+      }
+    });
+    // ----------------------
     this.getProfileImage(this.author.$key);
     this.getArticleCoverImage(this.articleData.$key);
   }
@@ -54,4 +68,26 @@ export class ArticleSearchPreviewComponent implements OnInit {
       }
     });
   }
+
+  // ---------------------------------------
+  checkIfBookmarked() {
+    this.articleSvc.isBookmarked(this.user.$key, this.articleData.$key).subscribe(bookmark => {
+      this.isArticleBookmarked = bookmark;
+      console.log(bookmark);
+      
+    })
+  }
+
+  bookmarkToggle() {
+    this.authSvc.isLoggedInCheck().subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        if (this.isArticleBookmarked)
+          this.articleSvc.unBookmarkArticle(this.user.$key, this.articleData.$key);
+        else
+          this.articleSvc.bookmarkArticle(this.user.$key, this.articleData.$key);
+      }
+    })
+  }
+  // ---------------------------------------
+
 }
