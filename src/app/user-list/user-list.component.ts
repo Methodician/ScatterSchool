007e5a120e66825d5cd61dfbe1a8831781b62057
Aubrey Masten <mastenaub@gmail.com>
2017-10-16 12:48:17 -0700

    update user list styles format

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -6,7 +6,7 @@ import { UserService } from './../services/user/user.service';
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
   @Input() userList;
