b5b9a2ab4c417a9eb65fdb0d3f9ce1276f8c9426
JordLoop <JordLoop@gmail.com>
2017-10-18 15:40:02 -0700

    add cloud function to make comment associations upon pushing a new comment to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,40 @@
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Docs: https://firebase.google.com/docs/functions/get-started
+// Use command 'firebase deploy --only functions' from root dir to deploy file changes the cloud
 
-// You must return a Promise when performing asynchronous tasks inside a
-// Functions such a writing to the Firebase Realtime Database.
+// Important: You must return a Promise when performing asynchronous tasks 
+// inside a functions such a writing to the Firebase Realtime Database.
+
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 exports.makeCommentAssociations = functions.database.ref('/commentData/comments/{pushId}')
     .onWrite(event => {
         const newCommentKey = event.data.key;
-        const newCommentVlaue = event.data.val();
-        // makeParentAssociation
-        functions.database.ref(`commentData/commentsPerParent/${ newComment.parentKey }/${ newCommentKey }`).set(true);
-
-        // makeUserAssociation
-        // makeParentTypeAssociation
-        // makeCommentsPerArticleAssociation
+        const newCommentValue = event.data.val();
+        console.log('commentValue', newCommentValue);
+        makeParentAssociation(newCommentValue.parentKey, newCommentKey)
+        makeUserAssociation(newCommentValue.authorKey, newCommentKey);
+        makeParentTypeAssociation(newCommentValue.parentType, newCommentValue.parentKey, newCommentKey)
     });
 
+function makeParentAssociation(parentKey, commentKey) {
+    return admin.database().ref(`commentData/commentsPerParent/${parentKey}/${commentKey}`).set(true);
+}
+
+function makeUserAssociation(authorKey, commentKey) {
+    return admin.database().ref(`commentData/commentsPerUser/${authorKey}/${commentKey}`).set(true);
+}
+
+function makeParentTypeAssociation(parentType, parentKey, childKey) {
+    if (parentType === 'article') return makeCommentsPerArticleAssociation(parentKey, childKey);
+    if (parentType === 'comment') return makeRepliesPerCommentAssociation(parentKey, childKey);
+}
+
+function makeCommentsPerArticleAssociation(parentKey, commentKey) {
+    return admin.database().ref(`commentData/commentsPerArticle/${parentKey}/${commentKey}`).set(true);
+}
+
+function makeRepliesPerCommentAssociation(parentKey, commentKey) {
+    return admin.database().ref(`commentData/repliesPerComment/${parentKey}/${commentKey}`).set(true);
+}
