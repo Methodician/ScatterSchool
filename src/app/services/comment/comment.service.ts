10b84036d69018b9af21c92c7c04e9fe6d76dc31
JordLoop <JordLoop@gmail.com>
2017-10-18 15:40:32 -0700

    remove comment association methods from comment.service

--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -20,13 +20,7 @@ export class CommentService {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
       lastUpdated: firebase.database.ServerValue.TIMESTAMP
     }
-
     let dbSaveData = this.db.list('commentData/comments').push(commentToSave);
-    
-    // this.makeParentAssociation(commentData.parentKey, dbSaveData.key);
-    this.makeUserAssociation(commentData.authorKey, dbSaveData.key);
-    this.makeParentTypeAssociation(commentData.parentType, commentData.parentKey, dbSaveData.key);
-    this.makeCommentsPerArticleAssociation(commentData.parentKey, dbSaveData.key);
   }
 
   updateComment(newCommentData) {
@@ -38,38 +32,20 @@ export class CommentService {
   }
 
   deleteComment(commentKey) {
-    this.db.object(`commentData/comments/${commentKey}`).update({isDeleted: true});
-  }
-
-  makeParentTypeAssociation(parentType, parentKey, childKey) {
-    if (parentType === 'article') this.makeCommentsPerArticleAssociation(parentKey, childKey);
-    if (parentType === 'comment') this.makeRepliesPerCommentAssociation(parentKey, childKey);
-  }
-
-  makeRepliesPerCommentAssociation(parentCommentKey, replyCommentKey) {
-    this.db.object(`commentData/repliesPerComment/${parentCommentKey}/${replyCommentKey}`).set(true);
-  }
-
-  getCommentsByParentKey(parentKey) {
-    return this.db.list(`commentData/comments`, {query: {
-      orderByChild: 'parentKey',
-      equalTo: parentKey
-    }});
-  }
-
-  makeCommentsPerArticleAssociation(articleKey, childCommentKey) {
-    this.db.object(`commentData/commentsPerArticle/${articleKey}/${childCommentKey}`).set(true);
-  }
-
-  // makeParentAssociation(parentKey, childCommentKey) {
-  //   this.db.object(`commentData/commentsPerParent/${parentKey}/${childCommentKey}`).set(true);
-  // }
-
-  makeUserAssociation(userKey, commentKey) {
-    this.db.object(`commentData/commentsPerUser/${userKey}/${commentKey}`).set(true);
+    this.db.object(`commentData/comments/${commentKey}`).update({ isDeleted: true });
   }
 
   getAllComments() {
     return this.db.list(`commentData/comments`)
   }
+
+  getCommentsByParentKey(parentKey) {
+    return this.db.list(`commentData/comments`, {
+      query: {
+        orderByChild: 'parentKey',
+        equalTo: parentKey
+      }
+    });
+  }
+
 }
