935193f9b4058319aef24468d28859e31fa8ae84
JordLoop <JordLoop@gmail.com>
2017-10-19 11:18:08 -0700

    replace commant association functions with one atomic function to make all command associations

--- functions/index.js
+++ functions/index.js
@@ -5,25 +5,45 @@
 // inside a functions such a writing to the Firebase Realtime Database.
 
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+const firebase = require('firebase-admin');
+firebase.initializeApp(functions.config().firebase);
 
+//-------------v CommentAssociations v-------------
 exports.makeCommentAssociations = functions.database.ref('/commentData/comments/{pushId}')
     .onWrite(event => {
         const newCommentKey = event.data.key;
         const newCommentValue = event.data.val();
-        console.log('commentValue', newCommentValue);
-        makeParentAssociation(newCommentValue.parentKey, newCommentKey)
-        makeUserAssociation(newCommentValue.authorKey, newCommentKey);
-        makeParentTypeAssociation(newCommentValue.parentType, newCommentValue.parentKey, newCommentKey)
+
+        commentAssociations({
+            rootRef: firebase.database().ref(),
+            commentKey: newCommentKey,
+            commentValue: newCommentValue
+        }).then(_ => {
+            console.log('Success: commentAssociations.');
+        }).catch(err => {
+            console.error('Fail: commentAssociations.', err)
+        });
     });
+    
+// This is an atomic function. As a guide I used
+// this video: https://www.youtube.com/watch?v=i1n9Kw3AORw starting at 5:35
+function commentAssociations({rootRef, commentKey, commentValue}) {
+    // Create empty update object
+    let updateObj = {}
+    //  Add to updateObj: The path and value you would like to update
+        updateObj[makeParentAssociation(commentValue.parentKey, commentKey)] = true;
+        updateObj[makeUserAssociation(commentValue.parentKey, commentKey)] = true;
+        updateObj[makeParentTypeAssociation(commentValue.parentType, commentValue.parentKey, commentKey)] = true;
+    // Updates the database, return the promise 
+    return rootRef.update(updateObj)
+}
 
 function makeParentAssociation(parentKey, commentKey) {
-    return admin.database().ref(`commentData/commentsPerParent/${parentKey}/${commentKey}`).set(true);
+    return `commentData/commentsPerParent/${parentKey}/${commentKey}`;
 }
 
 function makeUserAssociation(authorKey, commentKey) {
-    return admin.database().ref(`commentData/commentsPerUser/${authorKey}/${commentKey}`).set(true);
+    return `commentData/commentsPerUser/${authorKey}/${commentKey}`;
 }
 
 function makeParentTypeAssociation(parentType, parentKey, childKey) {
@@ -32,9 +52,10 @@ function makeParentTypeAssociation(parentType, parentKey, childKey) {
 }
 
 function makeCommentsPerArticleAssociation(parentKey, commentKey) {
-    return admin.database().ref(`commentData/commentsPerArticle/${parentKey}/${commentKey}`).set(true);
+    return `commentData/commentsPerArticle/${parentKey}/${commentKey}`;
 }
 
 function makeRepliesPerCommentAssociation(parentKey, commentKey) {
-    return admin.database().ref(`commentData/repliesPerComment/${parentKey}/${commentKey}`).set(true);
+    return `commentData/repliesPerComment/${parentKey}/${commentKey}`;
 }
+//-------------^ CommentAssociations ^-------------
