291f5545652bdc0527d2f674e8c7589cccac1dbd
Methodician <jacobgjohnston@gmail.com>
2017-10-19 12:40:26 -0700

    moved DB stuff over to service

--- src/app/firestore-testing.service.ts
+++ src/app/firestore-testing.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Injectable()
 export class FirestoreTestingService {
@@ -8,12 +8,26 @@ export class FirestoreTestingService {
     private afs: AngularFirestore
   ) { }
 
-  setTest(info) {
-    //this.getTest().
+  setTest(object) {
+    this.getDoc().set(object);
   }
 
-  getTest() {
-    const doc: AngularFirestoreDocument<any> = this.afs.doc('test');
+  updateTest(object) {
+    this.getDoc().update(object);
+  }
+
+  getDoc() {
+    const doc: AngularFirestoreDocument<any> = this.afs.doc<any>('test/2');
+    return doc;
+  }
+
+  addTest(object) {
+    this.getCollection().add(object);
+  }
+
+  getCollection() {
+    const collection: AngularFirestoreCollection<any> = this.afs.collection<any>('test');
+    return collection;
   }
 
 
--- src/app/firestore-testing/firestore-testing.component.ts
+++ src/app/firestore-testing/firestore-testing.component.ts
@@ -1,6 +1,7 @@
+import { FirestoreTestingService } from './../firestore-testing.service';
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-firestore-testing',
@@ -8,62 +9,49 @@ import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection 
   styleUrls: ['./firestore-testing.component.scss']
 })
 export class FirestoreTestingComponent implements OnInit {
-  item$: Observable<any>;
-  item: AngularFirestoreDocument<any>;
-  test: any;
+  docPath: string;
+  collectionKeys: any;
 
-  collection$: Observable<any>;
+  item$: Observable<any>;
+  document: AngularFirestoreDocument<any>;
+
+  itemColl$: Observable<any>;
+  collection: AngularFirestoreCollection<any>;
 
   constructor(
-    private afs: AngularFirestore
+    private ftSvc: FirestoreTestingService
   ) { }
 
   ngOnInit() {
-    //const itemDoc: AngularFirestoreDocument<any> = this.afs.doc<any>('test/2');
-    const itemDoc: AngularFirestoreDocument<any> = this.getTest();
-    this.test = itemDoc.snapshotChanges().subscribe(snap => {
-      //debugger
-      this.test = snap.payload.ref.path;
+    //const itemDoc: AngularFirestoreDocument<any> = this.afs.document<any>('test/2');
+    this.document = this.ftSvc.getDoc();
+    this.item$ = this.document.valueChanges();
+    this.document.snapshotChanges().subscribe(snap => {
+      this.docPath = snap.payload.ref.path;
     })
-    this.item$ = itemDoc.valueChanges();
-    //  Must set an object not a string
-    itemDoc.set(
+
+    this.document.set(
       {
         test: 'testing',
         thing: 'thinging'
       }
     );
 
-    const itemCol: AngularFirestoreCollection<any> = this.afs.collection<any>('test/3/foo');
-    this.collection$ = itemCol.valueChanges();
-
-    //itemCol.add({ foo: 'bar' });
+    this.collection = this.ftSvc.getCollection();
+    this.itemColl$ = this.collection.valueChanges();
+    this.collection.snapshotChanges().subscribe(snap => {
+      this.collectionKeys = snap.keys;
+    })
   }
 
   update(stuff: string) {
     const stuffObj = { stuff: stuff };
-    this.updateTest(stuffObj);
-  }
-
-  updateTest(info) {
-    //const itemDoc: AngularFirestoreDocument<any> = this.afs.doc<any>('test/2');
-    const itemDoc = this.getTest();
-    itemDoc.update(info);
-  }
-
-  getTest() {
-    const doc: AngularFirestoreDocument<any> = this.afs.doc<any>('test/2');
-    return doc;
+    this.ftSvc.updateTest(stuffObj);
   }
 
   add(item: string) {
     const itemObj = { foo: item };
-    this.addTest(itemObj);
-  }
-
-  addTest(object) {
-    const collection: AngularFirestoreCollection<any> = this.afs.collection<any>('test/3/foo');
-    collection.add(object);
+    this.ftSvc.addTest(itemObj);
   }
 
 }
