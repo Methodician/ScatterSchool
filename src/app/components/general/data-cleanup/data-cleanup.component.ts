674c551487ba0bc032631c24403e7f26bfb36c1d
Methodician <jacobgjohnston@gmail.com>
2017-11-20 16:46:45 -0800

    nothing

--- src/app/components/general/data-cleanup/data-cleanup.component.ts
+++ src/app/components/general/data-cleanup/data-cleanup.component.ts
@@ -1,5 +1,8 @@
 //import { DataCleanupService } from 'app/shared/services/data-cleanup.service';
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'app/shared/services/article/article.service';
+import { UserService } from 'app/shared/services/user/user.service';
+import { AuthService } from 'app/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-data-cleanup',
@@ -8,13 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataCleanupComponent implements OnInit {
 
-  //constructor(private dataCleanupSvc: DataCleanupService) { }
+  articles: any;
+  authInfo = null;
+  userInfo = null;
 
+  constructor(
+    private articleSvc: ArticleService,
+    userSvc: UserService,
+    authSvc: AuthService
+  ) {
+
+  }
   ngOnInit() {
   }
 
-  articleNodeIdToKey() {
-    //this.dataCleanupSvc.articleNodeIdToKey();
+  copyAllToFirestore() {
+    this.articleSvc.getAllArticles().subscribe(articles => {
+      this.articles = articles;
+      for (let article of this.articles) {
+        this.articleSvc.getArticleBodyByKey(article.bodyKey).subscribe(body => {
+          article.body = body.$value;
+        })
+      }
+    });
   }
 
 }
