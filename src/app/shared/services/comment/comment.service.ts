befcfc60682bffdb42de26e99174c66d6a02f6ad
Aubrey Masten <mastenaub@gmail.com>
2018-02-28 09:11:29 -0800

    update comment service to meet linter standards

--- src/app/shared/services/comment/comment.service.ts
+++ src/app/shared/services/comment/comment.service.ts
@@ -12,7 +12,7 @@ export class CommentService {
   ) {}
 
   saveComment(commentData) {
-    let commentToSave = {
+    const commentToSave = {
       authorKey: commentData.authorKey,
       parentKey: commentData.parentKey,
       parentType: commentData.parentType,
@@ -21,61 +21,75 @@ export class CommentService {
       lastUpdated: firebase.database.ServerValue.TIMESTAMP,
       commentCount: 0
     }
-    let dbSaveData = this.db.list('commentData/comments').push(commentToSave);
+    this.db.list('commentData/comments').push(commentToSave);
   }
 
   // this could be replaced with an enum
   getBasePathByParentType(parentType) {
-    switch(parentType) {
-      case "comment":
-        return "commentData/comments/"
-      case "article":
-        return "articleData/articles/"
+    switch (parentType) {
+      case 'comment':
+        return 'commentData/comments/'
+      case 'article':
+        return 'articleData/articles/'
       default:
         return false;
     }
   }
 
   updateCommentCount(parentKey, parentType, value) {
-    let parentPath = this.getBasePathByParentType(parentType) + parentKey;
+    const parentPath = this.getBasePathByParentType(parentType) + parentKey;
 
     this.db.object(parentPath).query.ref.ref.transaction(parent => {
       if (parent) {
         // logic is verbose, but accounts for current data/data added which has not comment count
-        if (parent.commentCount) parent.commentCount += value;
-        else if (value == 1) parent.commentCount = 1;
-        else if (value == -1) parent.commentCount = 0; 
-        if(parentType == "comment") this.updateCommentCount(parent.parentKey, parent.parentType, value);
+        if (parent.commentCount) {
+          parent.commentCount += value;
+        } else if (value === 1) {
+          parent.commentCount = 1;
+        } else if (value === -1) {
+          parent.commentCount = 0;
+        }
+        if (parentType === 'comment') {
+          this.updateCommentCount(parent.parentKey, parent.parentType, value);
+        }
       }
       return parent;
-    });    
+    });
   }
 
   updateComment(newCommentData) {
-    let commentDataToUpdate = {
+    const commentDataToUpdate = {
       text: newCommentData.text,
       lastUpdated: firebase.database.ServerValue.TIMESTAMP
     }
-    this.db.object(`commentData/comments/${newCommentData.key}`).update(commentDataToUpdate)
+    this.db
+      .object(`commentData/comments/${newCommentData.key}`)
+      .update(commentDataToUpdate);
   }
 
   deleteComment(comment) {
-    this.db.object(`commentData/comments/${comment.$key}`).update({isDeleted: true});
+    this.db
+      .object(`commentData/comments/${comment.$key}`)
+      .update({isDeleted: true});
     this.updateCommentCount(comment.parentKey, comment.parentType, -1);
   }
 
   getAllComments() {
-    return this.db.list(`commentData/comments`)
+    return this.db.list(`commentData/comments`);
   }
 
   getCommentsByParentKey(parentKey: string) {
-    return this.db.list(`commentData/comments`,
-      ref => ref.orderByChild('parentKey').equalTo(parentKey)
-    ).snapshotChanges().map(comments => {
+    return this.db.list(`commentData/comments`, ref => {
+      return ref
+        .orderByChild('parentKey')
+        .equalTo(parentKey);
+    }).snapshotChanges().map(comments => {
       return comments.map(comment => {
-        return { $key: comment.key, ...comment.payload.val() }
+        return {
+          $key: comment.key,
+          ...comment.payload.val()
+        }
       })
     });
   }
-
 }
