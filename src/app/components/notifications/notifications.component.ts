183810e9aaeb74524ace1ed46c00e7657dcd44ca
Emily Wells Jimenez <emwellspdx@gmail.com>
2018-03-07 16:18:40 -0800

    end of aubrey guided help

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/services/user/user.service';
 
 @Component({
   selector: 'app-notifications',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent implements OnInit {
+  notifications: {}[];
 
-  constructor() { }
+  constructor(private userSvc: UserService) { }
 
   ngOnInit() {
-
+    this.userSvc
+      .getNotifications()
+      .valueChanges()
+      .subscribe(notifications => {
+        this.notifications = notifications;
+        console.log('notifications: ', notifications);
+      })
   }
 
 }
